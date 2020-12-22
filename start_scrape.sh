f9290c2d239ce7d579903d2af8345b102612fc9f
bchnge <bchnge@gmail.com>
2020-12-22 14:48:23 -0500

    update environment

--- start_scrape.sh
+++ start_scrape.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 session_name="scrape_nba"
+package_env_name="test3"
 
 tmux new -ds $session_name
+echo $package_env_name
+tmux send-keys -t $session_name "conda activate $package_env_name" C-m
 tmux send-keys -t $session_name 'python scrape.py' C-m
